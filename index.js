ee0892419ae9a5b731dc3ad87d5f64b034349a6f
sk <sajib.khan655@gmail.com>
2023-06-08 15:44:23 -0700

    products-filtering

---
+++ index.js
@@ -0,0 +1,120 @@
+const data = [
+    {
+      id: 1,
+      name: "Invicta Men's Pro Diver",
+      img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+      price: 74,
+      cat: "Dress",
+    },
+    {
+      id: 11,
+      name: "Invicta Men's Pro Diver 2",
+      img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+      price: 74,
+      cat: "Dress",
+    },
+    {
+      id: 2,
+      name: "Timex Men's Expedition Scout ",
+      img: "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
+      price: 40,
+      cat: "Sport",
+    },
+    {
+      id: 3,
+      name: "Breitling Superocean Heritage",
+      img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+      price: 200,
+      cat: "Luxury",
+    },
+    {
+      id: 4,
+      name: "Casio Classic Resin Strap ",
+      img: "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
+      price: 16,
+      cat: "Sport",
+    },
+    {
+      id: 5,
+      name: "Garmin Venu Smartwatch ",
+      img: "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_SL1000_.jpg",
+      price: 74,
+      cat: "Casual",
+    },
+  ];
+
+
+const productsContainer = document.querySelector(".products");
+const searchInput = document.querySelector(".search");
+const categoriesContainer = document.querySelector(".cats");
+const priceRange = document.querySelector(".priceRange");
+const priceValue = document.querySelector(".priceValue");
+
+
+const displayProducts = (products) => {
+    productsContainer.innerHTML =  products.map(
+        (product) => 
+        `
+            <div class="product">
+                <img src=${product.img} alt="" />
+                <span class="name">${product.name}</span>
+                <span class="priceText">$${product.price}</span>
+            </div>
+        `
+    ).join("")
+
+}
+
+
+displayProducts(data);
+
+
+searchInput.addEventListener("keyup", (e) => {
+
+    const userGivenValue =  e.target.value.toLowerCase();
+
+    if(userGivenValue) {
+        displayProducts(data.filter( (item) => item.name.toLocaleLowerCase().indexOf(userGivenValue) !== -1));
+    } else {
+        displayProducts(data)
+    }
+
+});
+
+
+setCategories = () => {
+    const allCats = data.map( (item) => item.cat);
+ 
+
+    const categories = ["All", ...allCats.filter((item,index) => {
+    
+        return allCats.indexOf(item) === index;
+    })];
+
+
+    categoriesContainer.innerHTML = categories.map((category) => 
+
+    `<span class="cat">${category}</span>`
+    
+    ).join("")
+
+
+    categoriesContainer.addEventListener("click", (e) => {
+
+        const selectedCat = e.target.textContent;
+
+        selectedCat === "All" ? displayProducts(data) : 
+        displayProducts(data.filter((item) => item.cat === selectedCat))
+    })
+  
+}
+
+setCategories()
+
+
+// const fruits = ["apple", "banana", "orange", "cocunut", "apple"];
+
+// fruits.filter((item,index) => {
+//     console.log(fruits.indexOf(item))
+// })
+
